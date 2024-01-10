edf46db7fc5d4bff6a285d8287207f02bc4e0ce5
slee3593 <slee3593@gatech.edu>
2024-01-10 17:52:04 +0900

    feat: import geoCoding

--- lib/ui/screen/weather_view_model.dart
+++ lib/ui/screen/weather_view_model.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/cupertino.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:weather_app/data/repository/weather_repository.dart';
 
-class WeatherViewModel extends ChangeNotifier{
- WeatherRepository _repository;
+class WeatherViewModel extends ChangeNotifier {
+  WeatherRepository _repository;
 
- WeatherViewModel({
+  String timeDate = '00:00 - 01/01';
+  double temperature = 0.0;
+  int humidity = 0;
+  double windSpeed = 0.0;
+  double pressure = 0.0;
+  int vmoCode = 0;
+
+
+  WeatherViewModel({
     required WeatherRepository repository,
   }) : _repository = repository;
 
+  Future getInfo() {
 
+  }
 
-
-
-
-
-
+  Future<List<Location>> convertLocationToLatLng(String location) async {
+    List<Location> locations = await locationFromAddress(location);
+    return locations;
+  }
 }
